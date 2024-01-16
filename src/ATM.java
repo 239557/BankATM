5912cf8eef8bb9680317706fb0a9f8b09c867c61
Tafsi Bhuiyan <tafsib@nycstudents.net>
2024-01-16 11:01:40 -0500

    Worked on the ATM class

--- src/ATM.java
+++ src/ATM.java
@@ -1,29 +1,53 @@
 import java.util.Scanner;
 public class ATM {
-    private static Scanner scan;
-    private static Customer customer;
+    private Customer customer;
     private Account account;
+    private TransactionHistory transactionHistory;
+    private Scanner scanner;
 
     public ATM() {
-        scan = new Scanner(System.in);
+        this.transactionHistory = new TransactionHistory();
+        this.scanner = new Scanner(System.in);
     }
 
-    public static void start() {
+    public void start() {
         welcome();
     }
 
-    private static void welcome() {
+    private void welcome() {
         System.out.println(ConsoleUtility.BLUE + "Welcome to the Bank ATM" + ConsoleUtility.RESET);
-        System.out.print("Please enter your name: ");
-        String name = scan.nextLine();
-        System.out.print("Please enter your PIN: ");
-        int pin = scan.nextInt();
-        scan.nextLine();
-        customer = new Customer(name, pin);
+
+    private void customer() {
+            System.out.print(ConsoleUtility.PURPLE + "Please enter your name: " + ConsoleUtility.RESET);
+            Scanner scan = null;
+            String name = scan.nextLine();
+            System.out.print(ConsoleUtility.RED + "Please enter your PIN: " + ConsoleUtility.RESET);
+            int pin = scan.nextInt();
+            scan.nextLine();
+            System.out.println(ConsoleUtility.YELLOW + "Would you like to make a (C)heckings account or a (S)aving account" + ConsoleUtility.RESET);
+            String answer = scan.nextLine();
+            if (answer.equals("c")) {
+                this.account = new Account("Checking Account", 0, customer);
+            } else if (answer.equals("s")) {
+                this.account = new Account("Savings Account", 0, customer);
+            }
+            System.out.println(ConsoleUtility.GREEN + "Account created" + ConsoleUtility.RESET);
+        }
     }
 
-    private void accountName() {
-        account = new Account();
+    private void mainMenu () {
+            boolean continueTransaction = true;
+            while (continueTransaction) {
+                System.out.println("Main Menu:");
+                System.out.println(ConsoleUtility.RED + "1." + ConsoleUtility.RESET + " Withdraw money");
+                System.out.println(ConsoleUtility.YELLOW + "2." + ConsoleUtility.RESET + " Deposit money");
+                System.out.println(ConsoleUtility.YELLOW + "3." + ConsoleUtility.RESET + " Transfer money between accounts");
+                System.out.println(ConsoleUtility.GREEN + "4." + " Get account balances");
+                System.out.println(ConsoleUtility.BLUE + "5." + ConsoleUtility.RESET + " Get transaction history");
+                System.out.println(ConsoleUtility.PURPLE + "6." + ConsoleUtility.RESET + " Change PIN");
+                System.out.println(ConsoleUtility.PURPLE + "7." + ConsoleUtility.RESET + " Exit");
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
+            }
+        }
     }
-
-}
